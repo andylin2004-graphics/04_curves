d11bd485541ca29ae9e1dacafa1d5cfad311c660
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-03-11 12:31:13 -0500

    main script caller done

--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,10 @@ mod color;
 mod draw;
 
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    let mut screen = Image::new(500, 500);
+    let color = Color::new_color(0, 255, 0);
+    let mut edges = Matrix::new(0,0);
+    let mut transform = Matrix::new(4,4);
+    parse_file("script", &mut edges, &mut transform, &mut screen, color);
 }
